f41187a1f5ab8aa0ee12c971b4f4660e25f949f3
Jiseltzambrano <jiseltzambrano@gmail.com>
2026-04-07 10:58:45 -0500

    main

---
+++ index.js
@@ -0,0 +1,9 @@
+const aprendices = [
+    { nombre: "Ana", nota: 4.5, programa: "ADSO" }, 
+    { nombre: "Luis",nota: 2.8, programa: "ADSO" }, 
+    { nombre: "Marta", nota: 3.7, programa: "Diseno Web" },
+    { nombre: "Pedro", nota: 1.9, programa: "ADSO" }, 
+    { nombre: "Sofia", nota: 5.0, programa:"Diseno Web" },
+    { nombre: "Gabriel", nota: 4.7, programa: "ADSO" }, 
+    { nombre: "Valentina", nota: 4.7, programa: "Diseno Web" } 
+];
