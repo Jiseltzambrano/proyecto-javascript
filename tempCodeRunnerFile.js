5910052cbecd1f1a54b03929e395d9f2fa14a995
Jiseltzambrano <jiseltzambrano@gmail.com>
2026-04-09 10:47:30 -0500

    funcion aprobados

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,27 @@
+const aprendices = [
+    { nombre: "Ana", nota: 4.5, programa: "ADSO" }, 
+    { nombre: "Luis",nota: 2.8, programa: "ADSO" }, 
+    { nombre: "Marta", nota: 3.7, programa: "Diseno Web" },
+    { nombre: "Pedro", nota: 1.9, programa: "ADSO" }, 
+    { nombre: "Sofia", nota: 5.0, programa:"Diseno Web" },
+    { nombre: "Nicol", nota: 4.0, programa:"Diseno Web" },
+    { nombre: "Gabriel", nota: 4.7, programa: "ADSO" }, 
+    { nombre: "Valentina", nota: 4.7, programa: "Diseno Web" } 
+];
+
+    function mostrarAprendices(aprendices){
+        const aprendicesADSO = aprendices.filter(apr=> apr.programa === "ADSO");
+        const aprendicesDW = aprendices.filter(apr=> apr.programa === "Diseno Web");
+        console.table(aprendicesADSO);
+        console.table(aprendicesDW);
+    }
+
+    mostrarAprendices(aprendices);
+
+    function mostrarAprendicesAprobados(aprendices){
+        const aprendicesAprobados = aprendices.filter(aprendiz=> aprendiz.nota >= 3.0);
+        console.log("===Aprendices Aprobados===");
+        console.table(aprendicesAprobados);
+    }
+
+    mostrarAprendicesAprobados(aprendices);
